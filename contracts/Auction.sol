939416ae16f02c2040bcacda6f4feb15ab82acde
Moscvin <vlad.moscvin@isa.utm.md>
2025-11-14 14:09:51 +0200

    Create timestamp for Auction and only seeler can take amount and finishing auction

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -11,6 +11,8 @@ contract Auction{
 
     uint endTime;
 
+    mapping (address => uint) chargeback;
+
     constructor(
     address payable _seller,
     uint auction_interval
@@ -18,15 +20,34 @@ contract Auction{
     seller = _seller;
     endTime = block.timestamp + auction_interval;
     }
-    
+
     function isEnded() public view returns (bool) {
     return block.timestamp > endTime; // Also replaced `now` (deprecated) with `block.timestamp`
     }
     function makeBid() public payable {
         require(!isEnded(), "Auction ended");
         require(msg.value >highestBid, "Invalid Bid");
+        if(highestBid != 0){
+            //last highestbidder need to take back money
+            chargeback[highestBidder] += highestBid;
+        }
 
         highestBidder = msg.sender;
         highestBid = msg.value;
+    }
+
+   function withdraw() public returns (bool) {
+    uint amount = chargeback[msg.sender];
+    if (amount > 0) {
+        payable(msg.sender).transfer(amount); // Reverts on failure
+        chargeback[msg.sender] = 0;
+        return true;
+    }
+    return false; // No funds to withdraw
+    }
+    function endAuction() public {
+        require(isEnded(), "Auction not ended");
+        require(msg.sender == seller, "Only seller can end auction");
+        seller.transfer(highestBid); // Reverts on failure
     }
 }
